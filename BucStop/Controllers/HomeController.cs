62b7a25b4ed7f57f7c4ef7cf1dcdb4ec5f01426d
Grayson Burke <burkegm@etsu.edu>
2024-10-01 12:44:19 -0400

    CJ-3

--- BucStop/Controllers/HomeController.cs
+++ BucStop/Controllers/HomeController.cs
@@ -65,6 +65,11 @@ namespace BucStop.Controllers
             return View();
         }
 
+        public IActionResult PreviousVersions()
+        {
+            return View();
+        }
+
         //If something goes wrong, this will take the user to a page explaining the error.
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
